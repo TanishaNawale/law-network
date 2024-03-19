496bf2f0e71c694ac3a2a8d2fbf2ef6aef826292
Pramod phad. <pramodphad02@gmail.com>
2024-03-19 14:41:15 +0530

    splash screen

---
+++ lib/presentation/splash_screen/demo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Demo extends StatelessWidget {
+  const Demo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: 
+      Column(
+        children: [
+Text("pramod")
+        ],
+      ),
+    );
+  }
+}
--- lib/util/routes/routing.dart
+++ lib/util/routes/routing.dart
@@ -1,22 +1,24 @@
-// import 'package:go_router/go_router.dart';
+import 'package:baap_market/presentation/splash_screen/demo.dart';
+import 'package:baap_market/presentation/splash_screen/splash_screen.dart';
+import 'package:go_router/go_router.dart';
 
-// class Routes {
-//   static String initialRoute = '/';
-//   static String homeScreen = '/';
-//   static String typeScreen = '/typeBaap-market';
-//   GoRouter get router => _goRouter;
+class Routes {
+  static String splashScreen = '/splash';
+  static String demo = '/demo';
+  static String typeScreen = '/typeBaap-market';
+  GoRouter get router => _goRouter;
 
-//   late final GoRouter _goRouter = GoRouter(
-//     initialLocation: homeScreen, // Corrected initialLocation
-//     routes: [
-//       GoRoute(
-//         path: homeScreen,
-//         builder: (context, state) => (),
-//       ),
-//       GoRoute(
-//         path: typeScreen,
-//         builder: (context, state) => (),
-//       ),
-//     ],
-//   );
-// }
+  late final GoRouter _goRouter = GoRouter(
+    initialLocation: splashScreen, // Corrected initialLocation
+    routes: [
+      GoRoute(
+        path: splashScreen,
+        builder: (context, state) => SplashScreen(),
+      ),
+      GoRoute(
+        path:demo ,
+        builder: (context, state) => Demo(),
+      ),
+    ],
+  );
+}
